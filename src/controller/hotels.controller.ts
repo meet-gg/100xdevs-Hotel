f600ff6ecebb2b66ce7715af2cd1a26b8ea774fd
meet-gg <meetgoyanip@gmail.com>
2026-02-11 20:46:46 +0530

    add hotel filter

--- src/controller/hotels.controller.ts
+++ src/controller/hotels.controller.ts
@@ -120,7 +120,75 @@ const createRoom = async (req: Request, res: Response) => {
 const getHotels = async (req: Request<{}, {}, {}, hotelQueryType>, res: Response) => {
 
     const { city, country, minPrice, maxPrice, minRating } = req.query;
-    
+
+    const minP = minPrice ? Number(minPrice) : undefined;
+    const maxP = maxPrice ? Number(maxPrice) : undefined;
+    const minR = minRating ? Number(minRating) : undefined;
+
+    const pricePerNightFilter: any = {};
+    if (minP !== undefined) pricePerNightFilter.gte = minP;
+    if (maxP !== undefined) pricePerNightFilter.lte = maxP;
+
+    const priceRoomFilter =
+        Object.keys(pricePerNightFilter).length > 0
+            ? { pricePerNight: pricePerNightFilter }
+            : {};
+
+    const hotels = await prisma.hotel.findMany({
+        where: {
+            ...(city && {
+                city: { contains: city, mode: "insensitive" }
+            }),
+
+            ...(country && {
+                country: { contains: country, mode: "insensitive" }
+            }),
+
+            ...(minR !== undefined && {
+                rating: { gte: minR }
+            }),
+
+            rooms: {
+                some: priceRoomFilter
+            }
+        },
+
+        select: {
+            id: true,
+            name: true,
+            description: true,
+            city: true,
+            country: true,
+            amenities: true,
+            rating: true,
+            totalReviews: true,
+
+            rooms: {
+                where: priceRoomFilter,
+                select: {
+                    pricePerNight: true
+                }
+            }
+        }
+    });
+
+
+    const data = hotels.map(hotel => ({
+        id: hotel.id,
+        name: hotel.name,
+        description: hotel.description,
+        city: hotel.city,
+        country: hotel.country,
+        amenities: hotel.amenities,
+        rating: hotel.rating,
+        totalReviews: hotel.totalReviews,
+        minPricePerNight: Math.min(
+            ...hotel.rooms.map(room => room.pricePerNight)
+        )
+    }));
+
+    return res.status(200).json(new ApiResponse(200, data));
+
 }
 
 const getHotelById = async (req: Request<{ hotelId: string }>, res: Response) => {
